05045e24f98d5ba48686529016810a059ef01b78
wangxizhong <1025514023@qq.com>
2017-05-08 11:38:08 +0800

    commit build project by gradle

---
+++ src/main/java/wxfm/ConfigurationCenterClientApplication.java
@@ -0,0 +1,12 @@
+package wxfm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigurationCenterClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigurationCenterClientApplication.class, args);
+	}
+}
